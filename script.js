2a49804491a67de776e59c513dca14afccbbd5e0
lam-code <l.laarej@codeur.online>
2022-03-02 17:51:21 +0100

    rajout CSS

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
+const btnAjouter = document.getElementById("btnAjouter");
+const champ = document.getElementById("champ");
+const list = document.getElementById("list");
+
+btnAjouter.addEventListener("click",list);
+
+
 form.onsubmit = () =>{
     const li = document.createElement("li");
     li.textContent = champ.value ;
+   
+
+    // bouton editer
+    const editButton = document.createElement("button");
+    editButton.innerText = "Editer";
+    editButton.classList.add("edit-btn");
+    list.appendChild(editButton);
+
+    // bouton supprimer
+
+    const suppButton = document.createElement("button");
+    suppButton.innerText = "Supprimer";
+    suppButton.classList.add("supp-btn");
+    list.appendChild(suppButton);
+    li.appendChild(editButton, suppButton);
+
     list.appendChild(li);
     champ.value="";
     return false;
 
-    // bouton supprimer
-    const completeButton = document.createElement("button");
-    completeButton.innerHTML = "Editer";
-    completeButton.classList.add("complet-btn");
-    list.appendChild(completeButton);
+    editButton.addEventListener('click',()=>{
+        
+
+
+
+    })
+
 }
 
 
