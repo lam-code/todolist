9f4e5a23b9a9288eb43e7c03dd93399e13e4a698
lam-code <l.laarej@codeur.online>
2022-03-10 17:58:22 +0100

    rajout input todolist

--- script.js
+++ script.js
@@ -2,51 +2,53 @@ const btnAjouter = document.getElementById("btnAjouter");
 const champ = document.getElementById("champ");
 const list = document.getElementById("list");
 
-btnAjouter.addEventListener("click",list);
+// btnAjouter.addEventListener("click", list);
 
+form.onsubmit = () => {
+  const li = document.createElement("li");
+  li.textContent = champ.value;
 
-form.onsubmit = () =>{
-    const li = document.createElement("li");
-    li.textContent = champ.value ;
-   
+  // bouton editer
+  const editButton = document.createElement("button");
+  editButton.innerText = "Editer";
+  editButton.classList.add("edit-btn");
+//   li.appendChild(editButton);
 
-    // bouton editer
-    const editButton = document.createElement("button");
-    editButton.innerText = "Editer";
-    editButton.classList.add("edit-btn");
-    list.appendChild(editButton);
-    
-    
-    editButton.addEventListener("click", function modifier(e) {
-        // e.preventDefault();
-        const addInput = document.createElement("input");
-        addInput.type = "text";
-        addInput.innerText = li.value;
-        li.appendChild(addInput);
-        
-    });
+  // bouton supprimer
 
-    // bouton supprimer
+  const suppButton = document.createElement("button");
+  suppButton.innerText = "Supprimer";
+  suppButton.classList.add("supp-btn");
+  //   list.appendChild(suppButton);
+ 
+  suppButton.addEventListener("click", supprimer);
+  function supprimer() {
+    li.remove();
+  }
+  const addInput = document.createElement("input");
+  addInput.type = "text";
+  addInput.value = "maman";
+  // addInput.setAttribute("readonly",true);
+  console.log(addInput);
+  
+  li.append(addInput, editButton, suppButton);
+  // li.appendChild(editButton);
+  // li.appendChild(suppButton);
+  list.appendChild(li);
+  champ.value = "";
 
-    const suppButton = document.createElement("button");
-    suppButton.innerText = "Supprimer";
-    suppButton.classList.add("supp-btn");
-    list.appendChild(suppButton);
-    li.appendChild(editButton);
-    suppButton.addEventListener("click",supprimer);
-    li.appendChild(suppButton);
+  // editButton.addEventListener("click", () =>{
+  //       if (editButton.toLowerCase() == "Editer") {
+  //           addInput.removeAttribute("reandonly");
+  //           addInput.focus();
+  //           editButton.innerText ="enregistrer";
+  //           console.log("marche");
+  //       } else {
+  //           addInput.setAttribute("readonly" ,"readonly");
+  //           editButton.innerText = "modifier;"
+  //       }
 
-    function supprimer(){
-        li.remove();
-    }
-    
-
-    list.appendChild(li);
-    champ.value="";
-    return false;
+  // });
+//   editButton ();
+  return false;
 }
-
-
-
-
-
